e0426e5f9aa4b5829d864cac1140551f5446feb8
Aru09 <42914363+Aru09@users.noreply.github.com>
2019-05-07 02:59:31 +0600

    Add files via upload

---
+++ todo_back/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import TaskList, Task
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+
+    class Meta:
+        model = TaskList
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True)
+    status = serializers.CharField()
+    due_on = serializers.DateTimeField()
+    created_at = serializers.DateTimeField()
+    task_list = TaskListSerializer()
+
+
+class TasksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'status')
---
+++ todo_back/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import TaskLists, TaskListDetail, TaskDetail, TaskList1
+
+urlpatterns =[
+    path("task_list/", TaskLists.as_view(), name="task_list"),
+    #path("task_list/", task_list_list),
+    path("task_list/<int:task_list_id>/", TaskListDetail.as_view(), name="task_list_detail"),
+    #path("task_list/<int:task_list_id>/", task_list_detail, name="task_list_detail"),
+    path("task_list/<int:task_list_id>/tasks/", TaskLists, name="tasks_list"),
+    #path("tasks/<int:pk>/", TaskDetail.as_view(), name="task_detail")
+    path("tasks/<int:pk>/", TaskDetail.as_view(), name="task_detail"),
+    path("tasks/", TaskList1.as_view())
+]
